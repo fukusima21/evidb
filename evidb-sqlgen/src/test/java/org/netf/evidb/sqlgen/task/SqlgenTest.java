e350b76a8a3caa83aa9cd3798c15bd64d281def2
fukushima.k <fukushiam.k@opst.co.jp>
2019-07-03 22:07:29 +0900

    テストケースを修正

--- evidb-sqlgen/src/test/java/org/netf/evidb/sqlgen/task/SqlgenTest.java
+++ evidb-sqlgen/src/test/java/org/netf/evidb/sqlgen/task/SqlgenTest.java
@@ -1,11 +1,30 @@
 package org.netf.evidb.sqlgen.task;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.ResolverStyle;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.BlockJUnit4ClassRunner;
 
 @RunWith(BlockJUnit4ClassRunner.class)
 public class SqlgenTest {
+
+	@Test
+	public void test() throws Exception {
+
+		DateTimeFormatter formtter = DateTimeFormatter
+				.ofPattern("yyyy/M/d")
+				.withResolverStyle(ResolverStyle.STRICT);
+
+//		TemporalAccessor accessor = formtter.parse("2019/01/01");
+		LocalDate.parse("2019/01/01", formtter);
+
+
+
+//		System.out.println(accessor);
+	}
 
 	@Test
 	public void testExecute() throws Exception {
